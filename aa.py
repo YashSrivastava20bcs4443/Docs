36fe380011eef53b2cda85dbffb9218ec4852f39
Yash Srivastava <76934310+YashSrivastava20bcs4443@users.noreply.github.com>
2024-05-02 09:36:43 +0530

    Delete aa.py

--- aa.py
+++
@@ -1,9 +0,0 @@
-if re.search(r'fail(?:ed|ure)?', alert_description) and "request" in alert_description:
-    failures_menu_locator = "div[data-telemetryname='Menu-failures']"
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, failures_menu_locator))).click()
-    
-else:
-    # Default case
-    time.sleep(5)
-
-# Rest of the code...
