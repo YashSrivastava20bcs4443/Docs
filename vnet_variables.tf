ca3c94a20c107612b06129fded8f242bb0203025
Yash Srivastava <76934310+YashSrivastava20bcs4443@users.noreply.github.com>
2024-07-09 16:39:42 +0530

    Update vnet_variables.tf

--- vnet_variables.tf
+++ vnet_variables.tf
@@ -1,36 +1,29 @@
-# vnet_peering/variables.tf
-
-variable "source_vnet_id" {
-  description = "ID of the source VNet"
+variable "resource_group_name" {
+  description = "Name of the resource group where the VNets are deployed"
   type        = string
 }
 
-variable "source_vnet_name" {
-  description = "Name of the source VNet"
+variable "location" {
+  description = "Location where the VNets are deployed"
   type        = string
 }
 
-variable "source_resource_group_name" {
-  description = "Resource Group name of the source VNet"
+variable "vnet1_name" {
+  description = "Name of the first virtual network"
   type        = string
 }
 
-variable "destination_vnet_id" {
-  description = "ID of the destination VNet"
+variable "vnet1_address_space" {
+  description = "Address space of the first virtual network"
+  type        = list(string)
+}
+
+variable "vnet2_name" {
+  description = "Name of the second virtual network"
   type        = string
 }
 
-variable "destination_vnet_name" {
-  description = "Name of the destination VNet"
-  type        = string
-}
-
-variable "destination_resource_group_name" {
-  description = "Resource Group name of the destination VNet"
-  type        = string
-}
-
-variable "peering_name" {
-  description = "Name of the VNet peering"
-  type        = string
+variable "vnet2_address_space" {
+  description = "Address space of the second virtual network"
+  type        = list(string)
 }
