72046d6648426ed2b1bb83393c462b66d26cd7cf
Yash Srivastava <76934310+YashSrivastava20bcs4443@users.noreply.github.com>
2024-07-09 16:51:01 +0530

    Update vnet_variables.tf

--- vnet_variables.tf
+++ vnet_variables.tf
@@ -3,27 +3,12 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "location" {
-  description = "Location where the VNets are deployed"
-  type        = string
-}
-
 variable "vnet1_name" {
   description = "Name of the first virtual network"
   type        = string
 }
 
-variable "vnet1_address_space" {
-  description = "Address space of the first virtual network"
-  type        = list(string)
-}
-
 variable "vnet2_name" {
   description = "Name of the second virtual network"
   type        = string
-}
-
-variable "vnet2_address_space" {
-  description = "Address space of the second virtual network"
-  type        = list(string)
 }
