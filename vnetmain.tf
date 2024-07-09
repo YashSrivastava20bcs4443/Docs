0ab6fb2131eb33cf0fb8a36495217c752f4674a6
Yash Srivastava <76934310+YashSrivastava20bcs4443@users.noreply.github.com>
2024-07-09 16:50:00 +0530

    Update vnetmain.tf

--- vnetmain.tf
+++ vnetmain.tf
@@ -1,57 +1,44 @@
 variable "resource_group_name" {
   description = "Name of the resource group where the VNets are deployed"
-}
-
-variable "location" {
-  description = "Location where the VNets are deployed"
+  type        = string
 }
 
 variable "vnet1_name" {
   description = "Name of the first virtual network"
-}
-
-variable "vnet1_address_space" {
-  description = "Address space of the first virtual network"
+  type        = string
 }
 
 variable "vnet2_name" {
   description = "Name of the second virtual network"
-}
-
-variable "vnet2_address_space" {
-  description = "Address space of the second virtual network"
-}
-
-resource "azurerm_virtual_network" "vnet1" {
-  name                = var.vnet1_name
-  address_space       = var.vnet1_address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_virtual_network" "vnet2" {
-  name                = var.vnet2_name
-  address_space       = var.vnet2_address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  type        = string
 }
 
 resource "azurerm_virtual_network_peering" "vnet1-to-vnet2" {
-  name                      = "vnet1-to-vnet2"
+  name                      = "${var.vnet1_name}-to-${var.vnet2_name}"
   resource_group_name       = var.resource_group_name
-  virtual_network_name      = azurerm_virtual_network.vnet1.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+  virtual_network_name      = var.vnet1_name
+  remote_virtual_network_id = data.azurerm_virtual_network.vnet2.id
   allow_forwarded_traffic   = true
   allow_gateway_transit     = false
   use_remote_gateways       = false
 }
 
 resource "azurerm_virtual_network_peering" "vnet2-to-vnet1" {
-  name                      = "vnet2-to-vnet1"
+  name                      = "${var.vnet2_name}-to-${var.vnet1_name}"
   resource_group_name       = var.resource_group_name
-  virtual_network_name      = azurerm_virtual_network.vnet2.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+  virtual_network_name      = var.vnet2_name
+  remote_virtual_network_id = data.azurerm_virtual_network.vnet1.id
   allow_forwarded_traffic   = true
   allow_gateway_transit     = false
   use_remote_gateways       = false
+}
+
+data "azurerm_virtual_network" "vnet1" {
+  name                = var.vnet1_name
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet2" {
+  name                = var.vnet2_name
+  resource_group_name = var.resource_group_name
 }
