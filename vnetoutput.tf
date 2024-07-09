acc79b26aae91e1fc8931d9a7e52c20d72acedea
Yash Srivastava <76934310+YashSrivastava20bcs4443@users.noreply.github.com>
2024-07-09 16:51:27 +0530

    Update vnetoutput.tf

--- vnetoutput.tf
+++ vnetoutput.tf
@@ -1,7 +1,7 @@
-output "vnet1_id" {
-  value = azurerm_virtual_network.vnet1.id
+output "vnet1_to_vnet2_peering_id" {
+  value = azurerm_virtual_network_peering.vnet1-to-vnet2.id
 }
 
-output "vnet2_id" {
-  value = azurerm_virtual_network.vnet2.id
+output "vnet2_to_vnet1_peering_id" {
+  value = azurerm_virtual_network_peering.vnet2-to-vnet1.id
 }
