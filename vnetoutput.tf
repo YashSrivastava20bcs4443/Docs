fd71f353ccb66de9e940ad3ad6907debc8c6edf5
Yash Srivastava <76934310+YashSrivastava20bcs4443@users.noreply.github.com>
2024-07-11 10:35:07 +0530

    Delete vnetoutput.tf

--- vnetoutput.tf
+++
@@ -1,7 +0,0 @@
-output "vnet1_to_vnet2_peering_id" {
-  value = azurerm_virtual_network_peering.vnet1-to-vnet2.id
-}
-
-output "vnet2_to_vnet1_peering_id" {
-  value = azurerm_virtual_network_peering.vnet2-to-vnet1.id
-}
